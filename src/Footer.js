6ab93dbc85d5b938ca55600cbe68d2d6cf6908d8
Kumari Thotapalli <53396485+kumarith@users.noreply.github.com>
2020-07-06 13:23:45 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Footer(props){
     return(
         <div class="Footer">
+            <p>footer</p>
             <p>Designed and Developed By</p>
             <p>Kumari</p>
         </div>
